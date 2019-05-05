e0d39c958ee3a25ff8c3377971d39cf2ba2082cf
Serveny <dylan_lewis93@web.de>
2019-05-05 20:54:17 +0200

    Icons & Dark Mode

--- public/js/winklerbingo.js
+++ public/js/winklerbingo.js
@@ -9,6 +9,7 @@ class WinklerBingo {
         // Brobbaties
         this.Container = container;
         this.fieldChange = null;
+        this.isDarkMode = false;
         
         // Hadde Arbeit
         this.buildHTML();
@@ -23,7 +24,7 @@ class WinklerBingo {
         for (let i = 1; i < 6; i++) {
             for (let u = 1; u < 6; u++) {
                 fieldsHTML += '<div id="wB_' + ++count + '" class="wB_field" data-x="' + i + '" data-y="' + u + '" data-count="' + count + '">' +
-                '<span></span>' +
+                '<span class="wB_field_text"></span>' +
                 '</div>';
             }
         }
@@ -33,12 +34,18 @@ class WinklerBingo {
 
     // 20.08 Schanzenfest
     registerEvents() {
+        const _self = this;
+
         this.addTextFieldEvents();
+
+        $('#toggleDarkBtn').click(function(e) {
+            _self.toggleDarkMode(e);    
+        });
     }
 
     addTextFieldEvents() {
         const _self = this;
-        
+
         $('.wB_field').click(function() {
             if (_self.fieldChange != null) {
                 if ($(this).attr('id') == _self.fieldChange.attr('id')) {
@@ -46,7 +53,7 @@ class WinklerBingo {
                 } else {
                     _self.setTextToField(_self.fieldChange);
                 }
-            } 
+            }
             _self.addTextArea($(this));
         });
 
@@ -87,7 +94,7 @@ class WinklerBingo {
         $(document).click(function(e) {
             if (_self.fieldChange != null) {
                 let target = $(e.target);
-                if (target.hasClass('wB_field') === true || target.hasClass('wB_field_span') === true) {
+                if (target.hasClass('wB_field') === true || target.hasClass('wB_field_text') === true) {
                     return;
                 } else {
                     _self.setTextToField(_self.fieldChange);
@@ -102,7 +109,7 @@ class WinklerBingo {
 
         element
             .addClass('wB_field_focus')
-            .html('<textarea id="wB_fieldTextArea"></textarea>');
+            .html('<textarea id="wB_fieldTextArea" class="wB_field_text"></textarea>');
         
         $('#wB_fieldTextArea').focus().val(text);
     }
@@ -110,8 +117,20 @@ class WinklerBingo {
     setTextToField(element) {
         let text = element.find('textarea').val().trim();
         element.removeClass('wB_field_focus');
-        element.html('<span class="wB_field_span">' + text + '</span>');
+        element.html('<span class="wB_field_text">' + text + '</span>');
         this.fieldChange = null;
+    }
+
+    toggleDarkMode(e) {
+        this.isDarkMode = !this.isDarkMode;
+
+        if (this.isDarkMode === true) {
+            $('body').addClass('dark');
+            $('.wB_field').addClass('dark');
+        } else {
+            $('body').removeClass('dark');
+            $('.wB_field').removeClass('dark');
+        }
     }
 }
 
