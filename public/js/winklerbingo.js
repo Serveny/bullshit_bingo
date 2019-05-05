2ced4afa7356ed7b5421771f435a7bfe4af8d5eb
Serveny <dylan_lewis93@web.de>
2019-05-05 14:55:40 +0200

    Background-Noise & Tab/ Shift+Tab-Implementation

--- public/js/winklerbingo.js
+++ public/js/winklerbingo.js
@@ -18,10 +18,11 @@ class WinklerBingo {
     // HTML-Code positionieren
     buildHTML() {
         let fieldsHTML = '';
+        let count = 0;
         
         for (let i = 1; i < 6; i++) {
             for (let u = 1; u < 6; u++) {
-                fieldsHTML += '<div id="wB_' + i + '-' + u + '" class="wB_field">' +
+                fieldsHTML += '<div id="wB_' + ++count + '" class="wB_field" data-x="' + i + '" data-y="' + u + '" data-count="' + count + '">' +
                 '<span></span>' +
                 '</div>';
             }
@@ -32,8 +33,12 @@ class WinklerBingo {
 
     // 20.08 Schanzenfest
     registerEvents() {
-        const _self = this;
+        this.addTextFieldEvents();
+    }
 
+    addTextFieldEvents() {
+        const _self = this;
+        
         $('.wB_field').click(function() {
             if (_self.fieldChange != null) {
                 if ($(this).attr('id') == _self.fieldChange.attr('id')) {
@@ -45,15 +50,46 @@ class WinklerBingo {
             _self.addTextArea($(this));
         });
 
-        this.addTextCloseEvent();
-    }
+        $(document).on('keydown',function(e) {
+            let keyCode = e.keyCode || e.which;
 
-    addTextCloseEvent() {
-        const _self = this;
-        $(document).on('keypress',function(e) {
-            // Keypress: Enter
+            // Key: Tab
+            if (keyCode == 9) { 
+                e.preventDefault();
+                if (_self.fieldChange != null) { 
+                    let number = _self.fieldChange.attr('data-count');
+                    _self.setTextToField(_self.fieldChange);
+                    
+                    if(e.shiftKey) {
+                        number--;
+                    } else {
+                        number++;
+                    }
+
+                    let nextEl = $('#wB_' + number);
+                    if (nextEl.length >= 1) {
+                        _self.addTextArea(nextEl);
+                    } else {
+                        this.fieldChange = null;
+                    }
+                }
+            } 
+            
+            // Key: Enter
             if(e.which == 13) {
+                e.preventDefault();
                 if (_self.fieldChange != null) {
+                    _self.setTextToField(_self.fieldChange);
+                }
+            }
+        });
+
+        $(document).click(function(e) {
+            if (_self.fieldChange != null) {
+                let target = $(e.target);
+                if (target.hasClass('wB_field') === true || target.hasClass('wB_field_span') === true) {
+                    return;
+                } else {
                     _self.setTextToField(_self.fieldChange);
                 }
             }
@@ -63,15 +99,18 @@ class WinklerBingo {
     addTextArea(element) {
         this.fieldChange = element;
         let text = element.find('span').text();
-        element.html('<textarea id="wB_fieldTextArea">' + text + '</textarea>');
-        $('#wB_fieldTextArea').focus();
-    }
 
-    
+        element
+            .addClass('wB_field_focus')
+            .html('<textarea id="wB_fieldTextArea"></textarea>');
+        
+        $('#wB_fieldTextArea').focus().val(text);
+    }
 
     setTextToField(element) {
         let text = element.find('textarea').val().trim();
-        element.html('<span>' + text + '</span>');
+        element.removeClass('wB_field_focus');
+        element.html('<span class="wB_field_span">' + text + '</span>');
         this.fieldChange = null;
     }
 }
